b2a8181397435429a601f9935f3e05a510c6e7ef
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2023-12-25 13:34:30 +0530

    Update bird sprites movement

--- lib/component/bird.dart
+++ lib/component/bird.dart
@@ -1,4 +1,26 @@
 
-class Bird{
-  
+import 'package:flame/components.dart';
+import 'package:game/game/assets.dart';
+import 'package:game/game/bird_movement.dart';
+import 'package:game/game/flappy_bird_game.dart';
+
+class Bird extends SpriteGroupComponent<BirdMovement> with HasGameRef<FlappyBirdGame>{
+//   Constructor
+Bird();
+
+@override
+  Future<void> onLoad() async {
+   final birdMidFlap = await gameRef.loadSprite(Assets.birdMidFlap);
+   final birdUpFlap = await gameRef.loadSprite(Assets.birdUpFlap);
+   final birdDownFlap =await gameRef.loadSprite(Assets.birdDownFlap);
+
+   size = Vector2(50, 40);
+
+   current = BirdMovement.middle;
+   sprites = {
+     BirdMovement.middle : birdMidFlap,
+     BirdMovement.up : birdUpFlap,
+     BirdMovement.down : birdDownFlap
+   };
+}
 }
