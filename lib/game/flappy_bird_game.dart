996d52029dc3a0db11e46bd3234766e9782e88bd
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2023-12-25 13:36:55 +0530

    add bird object component in main

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -2,14 +2,19 @@
 
 import 'package:flame/game.dart';
 import 'package:game/component/background.dart';
+import 'package:game/component/bird.dart';
 import 'package:game/component/ground.dart';
 
 class FlappyBirdGame extends FlameGame{
+  // Object of Bird Component
+  late Bird bird;
+
   @override
   Future<void> onLoad() async{
     addAll([
       Background(),
-      Ground()
+      Ground(),
+      bird = Bird(),
     ]);
   }
 }
