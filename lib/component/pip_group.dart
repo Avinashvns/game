9f408e7b88c161e794ce6040c10620ae83d21619
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2023-12-25 14:01:48 +0530

    update position of pipe group

--- lib/component/pip_group.dart
+++ lib/component/pip_group.dart
@@ -1,14 +1,17 @@
 
 import 'package:flame/components.dart';
 import 'package:game/component/pipe.dart';
+import 'package:game/game/configuration.dart';
+import 'package:game/game/flappy_bird_game.dart';
 import 'package:game/game/pipe_position.dart';
 
-class PipeGroup extends PositionComponent {
+class PipeGroup extends PositionComponent with HasGameRef<FlappyBirdGame> {
 //   Constructor
 PipeGroup();
 
 @override
   Future<void> onLoad() async {
+  position.x = gameRef.size.x;
    addAll([
      Pipe(
      height: 200,
@@ -17,8 +20,15 @@ PipeGroup();
      Pipe(
          height: 300,
          pipePosition: PipePosition.bottom
-     )
+     ),
    ]);
+}
+
+@override
+  void update(double dt){
+  super.update(dt);
+  position.x -=Config.gameSpeed * dt;
+
 }
 
 }
