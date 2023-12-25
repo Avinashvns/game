7706aea55ed1f50550da7d7a0929851c5e6643cd
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2023-12-25 14:19:28 +0530

    Very Important add dynamic height of pipe group

--- lib/component/pip_group.dart
+++ lib/component/pip_group.dart
@@ -1,4 +1,6 @@
 
+import 'dart:math';
+
 import 'package:flame/components.dart';
 import 'package:game/component/pipe.dart';
 import 'package:game/game/configuration.dart';
@@ -9,16 +11,23 @@ class PipeGroup extends PositionComponent with HasGameRef<FlappyBirdGame> {
 //   Constructor
 PipeGroup();
 
+final _random = Random();
+
 @override
   Future<void> onLoad() async {
   position.x = gameRef.size.x;
+
+  final heightMinusGround = gameRef.size.y- Config.groundHeight;
+  final spacing = 100 + _random.nextDouble() * (heightMinusGround/4);
+  final centerY =spacing + _random.nextDouble() * (heightMinusGround - spacing);
+
    addAll([
      Pipe(
-     height: 200,
+     height: centerY - spacing/2,
      pipePosition: PipePosition.top
    ),
      Pipe(
-         height: 300,
+         height: heightMinusGround - (centerY + spacing/2),
          pipePosition: PipePosition.bottom
      ),
    ]);
