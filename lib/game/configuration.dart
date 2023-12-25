5c9736f0d46c30f5717b64de830d04776470c194
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2023-12-25 14:09:24 +0530

    Add timer utils in flame

--- lib/game/configuration.dart
+++ lib/game/configuration.dart
@@ -2,4 +2,7 @@
 class Config{
   static const gameSpeed = 200.0;
   static const groundHeight= 110.0;
+
+//   Pipe Interval
+ static const pipeInterval = 1.5;
 }
--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,14 +1,20 @@
 
 
+import 'dart:async';
+
 import 'package:flame/game.dart';
 import 'package:game/component/background.dart';
 import 'package:game/component/bird.dart';
 import 'package:game/component/ground.dart';
 import 'package:game/component/pip_group.dart';
+import 'package:flame/timer.dart';
+import 'package:game/game/configuration.dart';
 
 class FlappyBirdGame extends FlameGame{
   // Object of Bird Component
   late Bird bird;
+  Timer interval =Timer(Config.pipeInterval,repeat: true,
+  );
 
   @override
   Future<void> onLoad() async{
